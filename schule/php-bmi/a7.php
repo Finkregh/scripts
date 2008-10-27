b190576ce4957e3771ae1f15bbb75fd11de34261
Oluf Lorenzen <finkregh@mafia-server.net>
2008-10-27 23:58:21 +0000

    - whitespace fixing
    git-svn-id: https://gu-dev.mafia-server.net/svn/fink@10 da9769ef-4bad-4124-98ab-05abb145e92e

--- schule/php-bmi/a7.php
+++ schule/php-bmi/a7.php
@@ -19,14 +19,15 @@ include ('include.inc.php');
 htmlheader();
 
 function form() {
-	echo '				<h1>Aufgabe 7</h1>
-					<form action="' . $_SERVER[PHP_SELF] . '" method="post">
-                        <p>K&ouml;rpergr&ouml;&szlig;e <strong title="Required" class="required">*</strong>: <input type="text" name="groesse" value="'; if (isset($_POST["groesse"])) {echo $_POST["groesse"];} echo '" />cm<br />
-						<p><input type="submit"/></p>
-					</form>';
+	echo '<h1>Aufgabe 7</h1>
+	<form action="' . $_SERVER[PHP_SELF] . '" method="post">
+		<p>K&ouml;rpergr&ouml;&szlig;e <strong title="Required" class="required">*</strong>: <input type="text" name="groesse" value="'; if (isset($_POST["groesse"])) {echo $_POST["groesse"];} echo '" />cm<br />
+		<p><input type="submit"/></p>
+	</form> 
+	';
     if (!isset($_POST["groesse"]) || $_POST["groesse"] == '')
     {
-    	echo '					<p>Felder, die markiert <strong title="Required" class="required">*</strong> sind, m&uuml;ssen ausgef&uuml;llt werden!</p>';
+    	echo '			<p>Felder, die markiert <strong title="Required" class="required">*</strong> sind, m&uuml;ssen ausgef&uuml;llt werden!</p>';
     }
 }
 
@@ -44,15 +45,17 @@ else
 	
 	if ($groesse < 300) 
 	{
-		echo '				<h2>Ausgabe</h2>
-				<p>K&ouml;rpergr&ouml;&szlig;e: <em>' . $groesse . '</em>cm</p>
-				<ol>';
+		echo '<h2>Ausgabe</h2>
+	<p>K&ouml;rpergr&ouml;&szlig;e: <em>' . $groesse . '</em>cm</p>
+	<ol>
+		';
     	for ($counter=40; $counter<=90; $counter=$counter+10)
     	{
 			$bmi = round($counter / (($groesse / 100)* ($groesse / 100)), 1);
-			echo '					<li>bei einem Gewicht von: <em>' . $counter . 'kg</em> w&uuml;rde der BMI: <em>' . $bmi . '</em> betragen</li>';
+			echo '	<li>bei einem Gewicht von: <em>' . $counter . 'kg</em> w&uuml;rde der BMI: <em>' . $bmi . '</em> betragen</li>
+			';
 		}
-		echo '				</ol>';		
+		echo '</ol>';		
 	}
 	else
 	{
